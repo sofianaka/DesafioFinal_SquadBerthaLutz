f5f720a336ccd53bd63e2ec033ce6af4b124dbaa
Marina Caponera <m.caponera@gmail.com>
2024-10-18 16:32:02 -0300

    add: post detais e css

--- base/views.py
+++ base/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from .models import Postagens
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'base/post_list.html', {})
+    posts = Postagens.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'base/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Postagens, pk=pk)
+    return render(request, 'base/post_detail.html', {'post': post})
