3c150ed1f02dd320ea529073bbd688ca164e49ab
Marina Caponera <m.caponera@gmail.com>
2024-10-19 15:33:10 -0300

    fix: corrige problema no template

--- projeto_sistema/settings.py
+++ projeto_sistema/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
 
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
