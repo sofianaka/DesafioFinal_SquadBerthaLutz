7c17b892213f32529659777d87f34190a7367b81
Sofia Mayumi <mayuminakamaru1002@gmail.com>
2024-10-22 17:05:29 -0300

    mudando a parte de allowed_hosts

--- projeto_sistema/settings.py
+++ projeto_sistema/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-u+&aio66mxqew(n)=tziue7!xs534a-)x0bh3_k2)&a013rrww
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
