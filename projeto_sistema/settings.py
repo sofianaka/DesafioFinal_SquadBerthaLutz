5c003df0133310ff2d69d1bc74a13a5ed905d54d
Sofia Nakamaru <135164366+sofianaka@users.noreply.github.com>
2024-10-22 17:16:19 -0300

    Update settings.py
    configuracoes para fazer o deploy no render

--- projeto_sistema/settings.py
+++ projeto_sistema/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-u+&aio66mxqew(n)=tziue7!xs534a-)x0bh3_k2)&a013rrww
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,7 +122,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
-
+if not DEBUG:
+    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
